2461cc50b487cf0a99a3cc77d59ac5a0425a91fb
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-03-19 16:59:44 +0330

    fix: fixing categorizeChannel when dir being empty

--- utils.go
+++ utils.go
@@ -3,5 +3,8 @@ package gomq
 import "fmt"
 
 func categorizeChannel(channelDir, channel string) string {
+	if channelDir == "" {
+		return channel
+	}
 	return fmt.Sprintf("%s/%s", channelDir, channel)
 }
